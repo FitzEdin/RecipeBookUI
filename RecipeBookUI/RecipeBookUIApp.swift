073bdd86ea0cf2bf779c92c9446a9a5ec989d2c7
Fitz Edin <fitzroy.edinborough@gmail.com>
2023-09-19 17:44:26 -0400

    Initial Commit

---
+++ RecipeBookUI/RecipeBookUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeBookUIApp.swift
+//  RecipeBookUI
+//
+//  Created by Fitzroy Edinborough on 19/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct RecipeBookUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
